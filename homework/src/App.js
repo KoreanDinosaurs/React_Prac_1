ad3ccd75fc82800e1d2a43b88389b1ea0d5692d0
donghyunKang-dev <make_w@naver.com>
2022-03-21 11:19:52 +0900

    Container Setting

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,12 +1,37 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom"
+import styled from "styled-components"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+      <h1>내 일주일은?</h1>
+      <hr />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  min-width: 400px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 1em;
+  
+  
+  h1 {
+    text-align: center;
+  }
+  
+  hr {
+    /* margin: 2em; */
+    width: 100%;
+  }
+`;
